c740abceaec78bbeeb0860a971442ad96e4bf73a
David Steinberger <mail.steinberger@gmail.com>
2015-03-30 12:00:57 +0200

    add instructions for tern_for_vim install

--- post_install_message.sh
+++ post_install_message.sh
@@ -4,5 +4,5 @@
 echo
 echo
 echo "Start Vim with 'vim', ignore errors and run the command 'NeoBundleInstall'"
-echo "Quit Vim afterwards and run 'cd ~/.vim/bundle/YouCompleteMe; git submodule update --init --recursive; ./install.sh --clang-completer;'"
+echo "Quit Vim afterwards and run 'cd ~/.vim/bundle/YouCompleteMe; git submodule update --init --recursive; ./install.sh --clang-completer; cd ../tern_for_vim; npm install;'"
 echo "Start tmux with 'tmux', then run the command 'source-file ~/.tmux.conf'"
