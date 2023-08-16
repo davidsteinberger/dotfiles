8f5b94fac5e38b9a293482e84dbca8998e623e40
David Steinberger <mail.steinberger@gmail.com>
2023-08-16 22:17:18 +0200

    update config

--- LazyVim/lua/plugins/test.lua
+++ LazyVim/lua/plugins/test.lua
@@ -1,35 +1,19 @@
 return {
-  { "haydenmeade/neotest-jest" },
   {
     "nvim-neotest/neotest",
-    event = "VeryLazy",
     dependencies = {
-      "nvim-neotest/neotest-go",
-      "neotest-python",
       "haydenmeade/neotest-jest",
       -- Your other test adapters here
     },
-    keys = {},
     opts = {
       adapters = {
         ["neotest-jest"] = {
           jestCommand = "yarn test",
-          -- debug = true,
+          debug = true,
           -- env = { CI = true },
           cwd = function()
             return vim.fn.getcwd()
           end,
-        },
-        "neotest-go",
-        ["neotest-python"] = {
-          -- Extra arguments for nvim-dap configuration
-          -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-          dap = {
-            justMyCode = false,
-            console = "integratedTerminal",
-          },
-          args = { "--log-level", "DEBUG", "--quiet" },
-          runner = "pytest",
         },
       },
     },
