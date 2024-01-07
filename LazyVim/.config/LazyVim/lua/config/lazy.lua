8c9275e3127a8049a07f763a4250484d44e8d7a2
David Steinberger <mail.steinberger@gmail.com>
2024-01-07 13:49:52 +0100

    update config

--- LazyVim/.config/LazyVim/lua/config/lazy.lua
+++ LazyVim/.config/LazyVim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- LazyVim/.config/LazyVim/lua/plugins/navigation.lua
+++ LazyVim/.config/LazyVim/lua/plugins/navigation.lua
@@ -38,6 +38,13 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
     opts = {
       defaults = {
         mappings = {
@@ -55,9 +62,42 @@ return {
   },
   {
     "nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
     opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
         ["<CR>"] = cmp.mapping({
           i = function(fallback)
             if cmp.visible() and cmp.get_active_entry() then
@@ -106,5 +146,12 @@ return {
   },
   {
     "tpope/vim-rsi",
+  },
+  {
+    "gbprod/yanky.nvim",
+    keys = {
+      { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Cycle backward through yank history" },
+      { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Cycle forward through yank history" },
+    },
   },
 }
