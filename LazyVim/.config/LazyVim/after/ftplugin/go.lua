1f36f9bafebafaa68d01115f8b48b23ab5b3b485
David Steinberger <mail.steinberger@gmail.com>
2024-01-12 22:04:54 +0100

    update config

--- LazyVim/.config/LazyVim/after/ftplugin/go.lua
+++ LazyVim/.config/LazyVim/after/ftplugin/go.lua
@@ -1,10 +1,11 @@
 local map = vim.api.nvim_set_keymap
 DEFAULT_OPTIONS = { noremap = true, silent = true }
-map("n", "<Leader>cc", "compiler go | make -o /dev/null <CR><CR> | bufdo e! <CR>", DEFAULT_OPTIONS)
-map(
-  "n",
-  "<Leader>cg",
-  ":execute '!oapi-codegen -config api/config.yaml api/openapi.yaml' <CR><CR> | :LspRestart <CR>",
-  DEFAULT_OPTIONS
-)
+map("n", "<Leader>cc", ":compiler go | Make<CR>", DEFAULT_OPTIONS)
+-- map(
+--   "n",
+--   "<Leader>cg",
+--   ":execute '!oapi-codegen -config api/config.yaml api/openapi.yaml' <CR><CR> | :LspRestart <CR>",
+--   DEFAULT_OPTIONS
+-- )
+map("n", "<Leader>cg", ":wall! | !go generate ./...<CR>", DEFAULT_OPTIONS)
 map("n", "<Leader>ce", ":wall! | !go run ./...<CR>", DEFAULT_OPTIONS)
--- LazyVim/.config/LazyVim/after/ftplugin/typescript.tsx.lua
+++
@@ -1,5 +0,0 @@
-local map = vim.api.nvim_set_keymap
-DEFAULT_OPTIONS = { noremap = true, silent = true }
-map("n", "<Leader>cy", ":compiler tsc | setlocal makeprg=yarn\\ tsc\\ --noEmit<CR>", DEFAULT_OPTIONS)
-map("n", "<Leader>cn", ":compiler tsc | setlocal makeprg=npm\\ run\\ tsc\\ --noEmit<CR>", DEFAULT_OPTIONS)
-map("n", "<Leader>cc", ":Make<CR>", DEFAULT_OPTIONS)
