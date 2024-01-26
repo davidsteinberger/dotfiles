834a68f50df3be2af1e1e5f3d5d09cc3f9179154
David Steinberger <mail.steinberger@gmail.com>
2024-01-26 10:24:55 +0100

    update config

--- LazyVim/.config/LazyVim/lua/plugins/coding.lua
+++ LazyVim/.config/LazyVim/lua/plugins/coding.lua
@@ -123,13 +123,13 @@ return {
         ["<CR>"] = cmp.mapping({
           i = function(fallback)
             if cmp.visible() and cmp.get_active_entry() then
-              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false })
             else
               fallback()
             end
           end,
           s = cmp.mapping.confirm({ select = true }),
-          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
         }),
       })
     end,
