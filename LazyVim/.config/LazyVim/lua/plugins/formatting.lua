2aa1b2988145bc670e4331695b75b66e3d2c0147
David Steinberger <mail.steinberger@gmail.com>
2024-01-30 21:18:32 +0100

    update config

--- LazyVim/.config/LazyVim/lua/plugins/formatting.lua
+++ LazyVim/.config/LazyVim/lua/plugins/formatting.lua
@@ -2,20 +2,28 @@ return {
   {
     "stevearc/conform.nvim",
     opts = function(_, opts)
-      local is_pnp = vim.fn.findfile(".pnp.cjs", ".;") ~= ""
+      local pnp = vim.fn.findfile(".pnp.cjs", ".;")
+      local is_pnp = pnp ~= ""
       if is_pnp then
+        vim.fn.system("yarn bin prettier")
+        if vim.v.shell_error ~= 0 then
+          return
+        end
+        pnp = vim.fn.fnamemodify(pnp, ":p")
+        -- local root = pnp:gsub(".pnp.cjs", "")
         local yarn_bin = vim.fn.system("yarn bin prettier"):gsub("%s+", "")
         local prettier = require("conform.formatters.prettier")
         prettier.command = "node"
         prettier.args = {
           "--require",
-          "./.pnp.cjs",
+          -- root .. ".pnp.cjs",
+          pnp,
           yarn_bin,
           "--stdin-filepath",
           "$FILENAME",
         }
       end
-      opts.formatters_by_ft= vim.tbl_extend("force", opts.formatters_by_ft, {
+      opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft, {
         ["javascript"] = { { "prettier" } },
         ["javascriptreact"] = { { "prettier" } },
         ["typescript"] = { { "prettier" } },
