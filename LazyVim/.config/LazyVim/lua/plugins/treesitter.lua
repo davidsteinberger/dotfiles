080e5cb8a60e31e5c8e3a482864c87421689ea94
David Steinberger <mail.steinberger@gmail.com>
2024-10-18 15:45:24 +0200

    update config

--- LazyVim/.config/LazyVim/lua/plugins/treesitter.lua
+++ LazyVim/.config/LazyVim/lua/plugins/treesitter.lua
@@ -1,15 +1,18 @@
 return {
   {
     "nvim-treesitter",
-    opts = {
-      incremental_selection = {
+    opts = function(_, opts)
+      opts.incremental_selection = {
         enable = true,
         keymaps = {
           node_incremental = "v",
           node_decremental = "V",
           scope_incremental = "<M-v>",
         },
-      },
-    },
+      }
+      vim.list_extend(opts.ensure_installed, {
+        "graphql",
+      })
+    end,
   },
 }
