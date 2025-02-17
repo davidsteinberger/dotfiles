8fe62d4344da8c1e7e20d05c8db8fb5ea2425b8c
David Steinberger <mail.steinberger@gmail.com>
2025-02-17 09:00:40 +0100

    update config

--- LazyVim/.config/LazyVim/lua/plugins/writing.lua
+++ LazyVim/.config/LazyVim/lua/plugins/writing.lua
@@ -10,30 +10,6 @@ return {
         },
       },
     },
-    -- {
-    --   "nvimtools/none-ls.nvim",
-    --   opts = function(_, opts)
-    --     local nls = require("null-ls")
-    --     local sources = {}
-    --     for _, v in pairs(opts.sources) do
-    --       if v.name ~= "stylua" and v.name ~= "fish" and v.name ~= "shfmt" then
-    --         sources[#sources + 1] = v
-    --       end
-    --     end
-    --     opts.sources = vim.list_extend(sources, {
-    --       nls.builtins.diagnostics.proselint,
-    --       nls.builtins.code_actions.proselint,
-    --       nls.builtins.diagnostics.alex,
-    --       nls.builtins.diagnostics.write_good,
-    --     })
-    --   end,
-    -- },
-  },
-  {
-    "NStefan002/speedtyper.nvim",
-    branch = "main",
-    cmd = "Speedtyper",
-    opts = {},
   },
   {
     "epwalsh/obsidian.nvim",
