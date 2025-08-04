bc07f567ccfd068df69455c71292612fad213ca5
David Steinberger <mail.steinberger@gmail.com>
2025-08-04 08:30:22 +0200

    update config

--- zsh/.zaliases.zsh
+++ zsh/.zaliases.zsh
@@ -1,3 +1,15 @@
+# misc
+alias switch-keys="gpg-connect-agent \"scd serialno\" \"learn --force\" /bye"
+alias kr="defaults write -g ApplePressAndHoldEnabled -bool false"
+alias knr="defaults write -g ApplePressAndHoldEnabled -bool true"
+alias la="ls -al"
+alias lf="ls -al | grep ${1}"
+alias ls="ls -Gl"
+alias zx="source ~/.zshrc"
+alias zz="$EDITOR ~/.zshrc"
+alias ibrew='arch -x86_64 /usr/local/bin/brew'
+
+# git
 alias ga="git add ."
 alias gc="git commit -m ${1}"
 alias gd="git diff"
@@ -8,15 +20,10 @@ alias gco="git checkout ${1} ${2}"
 alias gpl="git pull ${1} ${2}"
 alias grb="git rebase ${1} ${2}"
 alias gs="git status"
-alias la="ls -al"
-alias lf="ls -al | grep ${1}"
-alias ls="ls -Gl"
-alias zx="source ~/.zshrc"
-alias zz="$EDITOR ~/.zshrc"
-alias kr="defaults write -g ApplePressAndHoldEnabled -bool false"
-alias knr="defaults write -g ApplePressAndHoldEnabled -bool true"
-alias switch-keys="gpg-connect-agent \"scd serialno\" \"learn --force\" /bye"
+
+# flutter
 alias flutter="fvm flutter"
+
 ## eza
 alias ls='eza --color=always --group-directories-first --icons=always'
 alias ll='eza -la --icons=always --octal-permissions --group-directories-first'
@@ -30,8 +37,6 @@ alias lt='eza --tree --level=2 --color=always --group-directories-first --icons=
 alias l.="eza -a | grep -E '^\.'"
 alias fzf="fzf --cycle  --multi --bind 'tab:toggle-up,btab:toggle-down'"
 
-alias ibrew='arch -x86_64 /usr/local/bin/brew'
-
 # nvim
 alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
 alias nvim-kick="NVIM_APPNAME=kickstart nvim"
@@ -44,3 +49,6 @@ alias k="k9s"
 
 # tmux
 alias tm="tmux new -A -s $(whoami)"
+
+# spotify
+alias spp="spotify_player"
