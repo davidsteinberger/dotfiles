9b30c6e53fb6744c9f167cf12d0f1983f7f1e0e4
David Steinberger <mail.steinberger@gmail.com>
2025-09-19 22:49:43 +0200

    update config

--- sketchybar/.config/sketchybar/colors.lua
+++ sketchybar/.config/sketchybar/colors.lua
@@ -1,28 +1,34 @@
 return {
-  black = 0xff181819,
-  white = 0xffe2e2e3,
-  red = 0xfffc5d7c,
-  green = 0xff9ed072,
-  blue = 0xff76cce0,
-  yellow = 0xffe7c664,
-  orange = 0xfff39660,
-  magenta = 0xffb39df3,
-  grey = 0xff7f8490,
-  transparent = 0x00000000,
+	black = 0xff181819,
+	white = 0xffe2e2e3,
+	red = 0xfff38ba8,
+	green = 0xff9ed072,
+	blue = 0xff76cce0,
+	yellow = 0xffe7c664,
+	orange = 0xfff39660,
+	magenta = 0xffb39df3,
+	grey = 0xff7f8490,
+	lavender = 0xffb4befe,
+	rosewater = 0xfff5e0dc,
+	text = 0xffcdd6f4,
+	overlay0 = 0xff9399b2,
+	transparent = 0x00000000,
 
-  bar = {
-    bg = 0xf02c2e34,
-    border = 0xff2c2e34,
-  },
-  popup = {
-    bg = 0xc02c2e34,
-    border = 0xff7f8490
-  },
-  bg1 = 0xff363944,
-  bg2 = 0xff414550,
+	bar = {
+		bg = 0x00000000,
+		border = 0xff2c2e34,
+	},
+	popup = {
+		bg = 0xc02c2e34,
+		border = 0xff7f8490,
+	},
+	bg1 = 0xff1e1e2e,
+	bg2 = 0xff9399b2,
 
-  with_alpha = function(color, alpha)
-    if alpha > 1.0 or alpha < 0.0 then return color end
-    return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
-  end,
+	with_alpha = function(color, alpha)
+		if alpha > 1.0 or alpha < 0.0 then
+			return color
+		end
+		return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
+	end,
 }
--- sketchybar/.config/sketchybar/items/aerospace.lua
+++ sketchybar/.config/sketchybar/items/aerospace.lua
@@ -171,7 +171,7 @@ local function onWorkspaceChanged(workspace, selected)
 			background = { border_color = selected and colors.black or colors.bg1 },
 		})
 		workspace.space_bracket:set({
-			background = { border_color = selected and colors.grey or colors.bg2 },
+			background = { border_color = selected and colors.lavender or colors.bg2 },
 		})
 	end)
 	updateWindows()
@@ -192,7 +192,7 @@ sbar.exec(query_workspaces, function(workspaces_and_monitors)
 			},
 			label = {
 				padding_right = 20,
-				color = colors.grey,
+				color = colors.lavender,
 				highlight_color = colors.white,
 				font = "sketchybar-app-font:Regular:16.0",
 				y_offset = -1,
@@ -231,69 +231,11 @@ sbar.exec(query_workspaces, function(workspaces_and_monitors)
 		end)
 	end
 
-	local spaces_indicator = sbar.add("item", {
-		padding_left = -3,
-		padding_right = 0,
-		icon = {
-			padding_left = 8,
-			padding_right = 9,
-			color = colors.grey,
-			string = icons.switch.on,
-		},
-		label = {
-			width = 0,
-			padding_left = 0,
-			padding_right = 8,
-			string = "Spaces",
-			color = colors.bg1,
-		},
-		background = {
-			color = colors.with_alpha(colors.grey, 0.0),
-			border_color = colors.with_alpha(colors.bg1, 0.0),
-		},
-	})
-
-	spaces_indicator:subscribe("swap_menus_and_spaces", function()
-		local currently_on = spaces_indicator:query().icon.value == icons.switch.on
-		spaces_indicator:set({
-			icon = currently_on and icons.switch.off or icons.switch.on,
-		})
-	end)
-
-	spaces_indicator:subscribe("mouse.entered", function()
-		sbar.animate("tanh", 30, function()
-			spaces_indicator:set({
-				background = {
-					color = { alpha = 1.0 },
-					border_color = { alpha = 1.0 },
-				},
-				icon = { color = colors.bg1 },
-				label = { width = "dynamic" },
-			})
-		end)
-	end)
-
-	spaces_indicator:subscribe("mouse.exited", function()
-		sbar.animate("tanh", 30, function()
-			spaces_indicator:set({
-				background = {
-					color = { alpha = 0.0 },
-					border_color = { alpha = 0.0 },
-				},
-				icon = { color = colors.grey },
-				label = { width = 0 },
-			})
-		end)
-	end)
-
-	spaces_indicator:subscribe("mouse.clicked", function()
-		sbar.trigger("swap_menus_and_spaces")
-	end)
-
 	local front_app = sbar.add("item", "front_app", {
 		display = "active",
 		icon = { drawing = false },
 		label = {
+			color = colors.text,
 			font = {
 				style = settings.font.style_map["Black"],
 				size = 12.0,
