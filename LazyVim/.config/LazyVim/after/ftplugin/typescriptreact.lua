b10434d696186cfa3c022282de4e6a54d6eaf6f1
David Steinberger <mail.steinberger@gmail.com>
2026-08-11 08:42:14 +0200

    update config

--- LazyVim/.config/LazyVim/after/ftplugin/typescriptreact.lua
+++ LazyVim/.config/LazyVim/after/ftplugin/typescriptreact.lua
@@ -5,19 +5,17 @@ local util = require("util")
 map("n", "<Leader>m", function()
   local root = util.find_root()
   local package_manager = util.detect_package_manager()
-  local tsc_cmd = package_manager == "npm" and "run tsc -- --noEmit" or "run tsc --noEmit"
-  local sed_root = root:gsub('["\\]', '\\%0')
+  local tsc_cmd = package_manager == "yarn" and "yarn tsc --noEmit" or "npx tsc --noEmit"
+  local sed_root = root:gsub('["\\]', "\\%0")
   local cmd = "cd "
     .. vim.fn.shellescape(root)
     .. " && "
-    .. util.fnm_exec()
-    .. " "
-    .. package_manager
+    .. util.mise_exec()
     .. " "
     .. tsc_cmd
     .. " 2>&1 | grep -E '^[^/][^(]+\\([0-9]+,[0-9]+\\): (error|warning)' | sed \"s|^|"
     .. sed_root
-    .. "/|\""
+    .. '/|"'
 
   util.make_async(cmd, {
     title = "tsc",
